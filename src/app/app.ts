0615e45416098734aafed93dbacfc6a9a053ebbe
Arthur Santiago <157769865+arthursant15@users.noreply.github.com>
2025-09-07 20:19:47 -0300

    chore: add material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,11 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './header/header';
+import { MatToolbar } from "@angular/material/toolbar";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, MatToolbar],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
--- src/app/header/header.ts
+++ src/app/header/header.ts
@@ -1,8 +1,8 @@
 import { Component } from '@angular/core';
-
+import { MatToolbarModule } from "@angular/material/toolbar";
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [MatToolbarModule],
   templateUrl: './header.html',
   styleUrl: './header.scss'
 })
